726f86eb89f0b94d08a90066da5b67c437725d4e
Murilo Araujo <murilo@muriloaraujo.com>
2023-06-10 17:43:22 -0300

    Initial Commit

---
+++ Life/LifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifeApp.swift
+//  Life
+//
+//  Created by Murilo Araujo on 10/06/23.
+//
+
+import SwiftUI
+
+@main
+struct LifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
